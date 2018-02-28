131d5a7a409297112b00e90b864410151eb3c393
Svetlio <yordanov_03@abv.bg>
2018-02-28 12:10:10 +0200

    Commit all

---
+++ C# OOP Basics/Defining Classes - Exercise/09. Rectangle Intersection/Rectangle Intersection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        var initialInput = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        int numberOfRectangles = initialInput[0];
+        int numberOfIntersections = initialInput[1];
+        var allRectangles = new List<Rectangle>();
+
+        for (int i = 0; i < numberOfRectangles; i++)
+        {
+            var separated = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            allRectangles.Add(new Rectangle(separated[0], double.Parse(separated[1]), double.Parse(separated[2]), double.Parse(separated[3]), double.Parse(separated[4])));
+        }
+        var pairsToIntersect = Console.ReadLine().Split(new[] { ' ' },StringSplitOptions.RemoveEmptyEntries).ToList();
+        var rectangle1 = allRectangles.Where(r => r.id == pairsToIntersect[0]).ToList();
+        var rectangle2 = allRectangles.Where(r => r.id == pairsToIntersect[1]).ToList();
+        
+
+
+    }
+   
+    }
+
---
+++ C# OOP Basics/Defining Classes - Exercise/09. Rectangle Intersection/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Rectangle
+    {
+    public string id;
+    public double width;
+    public double height;
+    public double coordinates1;
+    public double coordinates2;
+
+    public Rectangle(string Id, double Width, double Height, double Coordinates1, double Coordiantes2)
+    {
+        this.id = Id;
+        this.width = Width;
+        this.height = Height;
+        this.coordinates1 = Coordinates1;
+        this.coordinates2 = Coordiantes2;
+    }
+    public bool IntersectionChecker(Rectangle rectangle)
+    {
+        return rectangle.coordinates1 + rectangle.width >= this.coordinates1 &&
+            rectangle.coordinates1+rectangle.height>= this.coordinates1 &&
+            rectangle.coordinates2 + rectangle.width
+    }
+    }
+
